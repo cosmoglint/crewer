5f582c79d6ef624b519430bd691c4cf86e5ee43a
cosmoglint <ramprakashsmail@gmail.com>
2023-07-10 17:21:07 +0530

    ADD: auth framework and project manager application

--- crewer/ProjectManager/models.py
+++ crewer/ProjectManager/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_lengt=150)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
--- crewer/ProjectManager/views.py
+++ crewer/ProjectManager/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def projectlist(request):
+    HttpResponse("to-do: send a list of projects")
