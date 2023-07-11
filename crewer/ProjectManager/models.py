0e4b3b4bbf06b88c7f9d0a09870364bf323697ac
cosmoglint <ramprakashsmail@gmail.com>
2023-07-11 22:09:13 +0530

    EDIT: Project models with proper structure

--- crewer/ProjectManager/models.py
+++ crewer/ProjectManager/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Project(models.Model):
-    name = models.CharField(max_lengt=150)
+    name = models.CharField(max_length=150)
     description = models.TextField()
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
--- crewer/ProjectManager/views.py
+++ crewer/ProjectManager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def projectlist(request):
-    HttpResponse("to-do: send a list of projects")
+    return HttpResponse("to-do: send a list of projects")
