3f9fa71a61dd308e2fbdd817a10f5312550879bd
cosmoglint <ramprakashsmail@gmail.com>
2023-07-13 18:02:19 +0530

    EDIT: auth model changes and working apis

--- crewer/Auth/serializers.py
+++ crewer/Auth/serializers.py
@@ -1,11 +1,47 @@
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+from TaskManager.serializers import SkillSerializer
+from TaskManager.models import Skill
 
 User = get_user_model()
 
 # Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'skills']
+
+class ManageUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'role', 'tasks', 'skills']
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    skills = serializers.PrimaryKeyRelatedField(queryset=Skill.objects.all(), many=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'skills']
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        skill_data = validated_data.pop('skills')
+        print(skill_data)
+        for skill in skill_data:
+            user.skills.add(Skill.objects.get(name=skill))
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- crewer/Auth/views.py
+++ crewer/Auth/views.py
@@ -1,13 +1,81 @@
-from rest_framework import viewsets
-from .serializers import UserSerializer
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework import permissions, status
 from rest_framework.response import Response
+from ProjectManager.permissions import IsManager
+from rest_framework import generics
+from django.http import Http404
+from django.conf import settings
+from .serializers import RegisterUserSerializer, UserSerializer, ManageUserSerializer
+from .permissions import IsOwnerOrManager
 
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(APIView):
+    '''
+    Lists down all users
+    '''
+    permission_classes = [permissions.IsAuthenticated, IsManager]
+    def get(self, request):
+        users = User.objects.all()
+        serializer = ManageUserSerializer(users, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class UserProfileView(APIView):
+    '''
+    Shows the current user\'s profile to themselves
+    '''
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        current_user = request.user
+        serializer = UserSerializer(current_user, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetail(APIView):
+    '''
+    Detailed view selected user profile
+    '''
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrManager]
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id, format=None):
+        user = self.get_object(id)
+        if request.user.is_manager():
+            serializer = ManageUserSerializer(user)
+        else:
+            serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        user = self.get_object(id)
+        if request.user.is_manager():
+            serializer = ManageUserSerializer(user, data=request.data)
+        else:
+            serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterView(generics.CreateAPIView):
+    '''
+    Detailed view selected user profile
+    '''
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterUserSerializer
