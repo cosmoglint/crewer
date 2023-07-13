c8569048b570e8f059f174d520d5f0e0fe20d307
cosmoglint <ramprakashsmail@gmail.com>
2023-07-13 18:03:11 +0530

    EDIT: project apis and model changes

--- crewer/ProjectManager/views.py
+++ crewer/ProjectManager/views.py
@@ -10,26 +10,17 @@ from django.http import Http404
 from django.db.models import Q, Count
 from TaskManager.models import Task
 from Auth.models import User
+from TaskManager.serializers import TaskSerializer
 from .models import Project
 from .serializers import ProjectSerializer
 from .permissions import IsManager
 from .constants import ALL_RESOURCES_OCCUPIED
 
-def project_list(request):
-    if request.user.is_authenticated:
-        if request.user.role == settings.MANAGER:
-            return HttpResponse("to-do: send a list of projects")
-        elif request.user.role == settings.MEMBER:
-            return redirect(settings.TASK_TRACKER_URL)
-        else:
-            return HttpResponse('Unauthorized', status=401)
-    else:
-        return HttpResponse('Unauthorized', status=401)
 
 def check_resources_and_assign(task, resources):
+    # checks each available resource and assigns the task based on the resource availability
     required_skills = task.skills.all()
-    # q_required_skills = [Q(skills=obj) for obj in required_skills]
-    # skilled_users = resources.filter(Q(*q_required_skills, _connector=Q.AND))
+    # gets all available resources with the particular skill set
     skilled_users = resources.filter(skills__in=required_skills).annotate(num_skills=Count('skills')).filter(num_skills=len(required_skills))
     task_assigned = False
     for skilled_user in skilled_users:
@@ -47,30 +38,53 @@ def allocate_tasks(request, project):
     request_user = request.user
     assignment_success_list = []
     assignment_failure_list = []
-    if request_user.is_authenticated and request_user.role == settings.MANAGER:
-        unassigned_project_tasks = Task.objects.prefetch_related('skills').filter(project=project, status=settings.UNASSIGNED)
-        resources = User.objects.filter(role=settings.MEMBER)
-        for task in unassigned_project_tasks:
-            assigned, reason = check_resources_and_assign(task, resources)
-            response = {
-                'task': task.name,
-                'reason': reason,
-                'success': assigned
+    if Project.objects.filter(id=project).exists():
+        if request_user.is_authenticated and request_user.is_manager():
+            unassigned_project_tasks = Task.objects.prefetch_related('skills').filter(project=project, status=settings.UNASSIGNED)
+            if len(unassigned_project_tasks) == 0:
+                return JsonResponse({
+                    'successful': json.dumps(assignment_success_list),
+                    'failed': json.dumps(assignment_failure_list),
+                    'message': 'No more tasks left to be assigned'
+                })
+            resources = User.objects.filter(role=settings.MEMBER)
+            for task in unassigned_project_tasks:
+                assigned, reason = check_resources_and_assign(task, resources)
+                response = {
+                    'task': task.name,
+                    'reason': reason,
+                    'success': assigned
+                }
+                if assigned:
+                    assignment_success_list.append(response)
+                else:
+                    assignment_failure_list.append(response)
+            assignment_response = {
+                'successful': json.dumps(assignment_success_list),
+                'failed': json.dumps(assignment_failure_list),
+                'message': 'assignment complete!'
             }
-            if assigned:
-                assignment_success_list.append(response)
-            else:
-                assignment_failure_list.append(response)
-        assignment_response = {
-            'success': json.dumps(assignment_success_list),
-            'failed': json.dumps(assignment_failure_list)
-        }
-        return JsonResponse(assignment_response)
+            return JsonResponse(assignment_response)
+        else:
+            return HttpResponse("unauthorized", status=401)
     else:
-        return HttpResponse("unauthorized", status=401)
+        return HttpResponse("project does not exist", status=404) 
 
+
+class ProjectTaskList(APIView):
+    '''
+    list view of all tasks associated with a particular project
+    '''
+    permission_classes = [permissions.IsAuthenticated, IsManager]
+    def get(self, request, pk):
+        tasks = Task.objects.filter(project=pk)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
 
 class ProjectList(APIView):
+    '''
+    lists all projects available
+    '''
     permission_classes = [permissions.IsAuthenticated, IsManager]
     def get(self, request, format=None):
         projects = Project.objects.all()
@@ -85,6 +99,9 @@ class ProjectList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProjectDetail(APIView):
+    '''
+    Detailed view of a specific project
+    '''
     permission_classes = [permissions.IsAuthenticated, IsManager]
     def get_object(self, pk):
         try:
